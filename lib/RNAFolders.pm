2678824a076045314981af38db26c8bacd40e990
trey <trey>
2005-07-05 20:43:59 +0000

    wtf

--- lib/RNAFolders.pm
+++ lib/RNAFolders.pm
@@ -21,20 +21,13 @@ sub Nupack {
   chomp $tmp_dir;
   $tmp_dir .= '/work';
   chdir $tmp_dir;
-  print "TEST1: $input<br>\n";
 #  my $command = "$tmp_dir/Nupack $input";
   chdir("/home/trey/dinman/code/browser/work");
   my $command = "/home/trey/dinman/code/browser/work/Nupack $input";
-  open(NU, "$command |-") or die "Nupack failed $!.";
-  if (!defined($child_pid)) {
-	die "Could not fork a child process for nupack. $!<br>\n";
-  }
-  else {
-	## The child process does its work here.
-	while (my $line = <NU>) {
-	  chomp $line;
-	  print WRITER "$line<br>\n";
-	}
+  open(NU, "./Fold.out $input |") or die "Nupack failed $!.";
+  while (my $line = <NU>) {
+	chomp $line;
+	print WRITER "$line<br>\n";
   }
 }
 
