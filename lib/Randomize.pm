95eaca23efde4e38c3641f6b6a19acf0557ce23d
trey <trey>
2005-07-12 20:47:30 +0000

    rand

---
+++ lib/Randomize.pm
@@ -0,0 +1,21 @@
+package Randomize;
+use strict;
+
+sub new {
+  my ($class, %arg) = @_;
+  my $me = bless {
+				 }, $class;
+  return($me);
+}
+
+
+
+sub Coin_Random {
+  my $me = shift;
+  my $sequence = shift;
+  
+}
+
+
+
+1;
