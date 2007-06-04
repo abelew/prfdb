93acb6a6e9cc304346d04a1739066b36e06a09e1
trey <trey>
2007-06-04 20:10:10 +0000

    played with make_landscapes

--- contrib/make_pictures.pl
+++ contrib/make_pictures.pl
@@ -2,19 +2,24 @@
 use lib '../lib';
 use PRFConfig;
 use PRFdb;
-use Overlap;
+use Landscape;
 
 my $config = $PRFConfig::config;
 my $db = new PRFdb;
 my $land = new Landscape;
 
-my $accessions = qq(SELECT accession FROM landscape);
-my $acc = $db->MySelect($accession);
+my $accessions = qq(SELECT distinct(accession) FROM landscape);
+my $acc = $db->MySelect($accessions);
 foreach my $a (@{$acc}) {
   my $accession = $a->[0];
+  print "Working on $accession\n";
   my $test_dir = $land->Make_Directory($accession);
   my $test_file = qq($test_dir/$accession.png);
-  next $a if (-r $test_file);
-  $land->Make_Picture($accession);
+  if (-r $test_file) {
+   print "Already have $test_file\n";
+   next;
+  }
+  my $filename = $land->Make_Picture($accession);
+  print "Wrote $filename\n";
 }
 
