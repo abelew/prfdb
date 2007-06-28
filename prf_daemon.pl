3b7cb616631cd2e3d95988cb8dc960fd82acf80b
trey <trey>
2007-06-28 20:34:55 +0000

    removed make_jobs

--- prf_daemon.pl
+++ prf_daemon.pl
@@ -69,6 +69,7 @@ GetOptions(
   'stem2_spacer_min:i'     => \$conf{stem2_spacer_min},
   'stem2_spacer_max:i'     => \$conf{stem2_spacer_max},
   'checks:i'               => \$conf{checks},
+  'make_jobs'              => \$conf{make_jobs},
 );
 foreach my $opt ( keys %conf ) {
   if ( defined( $conf{$opt} ) ) {
@@ -140,6 +141,10 @@ if ( defined( $config->{input_file} ) ) {
   } else {
     Read_Accessions( $config->{input_file} );
   }
+}
+
+if ( defined($config->{make_jobs})) {
+    Make_Jobs();  ## USED FOR PBS SYSTEMS
 }
 
 if (defined($config->{accession})) {
@@ -686,6 +691,46 @@ sub Make_Blast {
   my $blast = new PRF_Blast;
   print "Formatting Database\n";
   $blast->Format_Db($outputfile);
+}
+
+sub Make_Jobs {
+    use lib "$ENV{HOME}/usr/perl.irix/lib";
+    use Template;
+    use PRFConfig;
+    my $template_config = $config;
+    $template_config->{PRE_PROCESS} = undef;
+    $template_config->{EVAL_PERL}   = 0;
+    $template_config->{INTERPOLATE} = 0;
+    $template_config->{POST_CHOMP}  = 0;
+    my $template = new Template($template_config);
+
+    my $base       = $template_config->{base};
+    my $prefix     = $template_config->{prefix};
+    my $input_file = "$prefix/descr/job_template";
+    my @arches     = split( / /, $config->{pbs_arches} );
+    foreach my $arch (@arches) {
+	system("mkdir jobs/$arch") unless ( -d "jobs/$arch" );
+	foreach my $daemon ( "01" .. $config->{num_daemons} ) {
+	    my $output_file  = "jobs/$arch/$daemon";
+	    my $name         = $template_config->{pbs_partialname};
+	    my $pbs_fullname = "${name}${daemon}_${arch}";
+	    my $incdir       = "${base}/usr/perl.${arch}/lib";
+	    my $vars         = {
+		pbs_shell   => $template_config->{pbs_shell},
+		pbs_memory  => $template_config->{pbs_memory},
+		pbs_cpu     => $template_config->{pbs_cpu},
+		pbs_arch    => $arch,
+		pbs_name    => $pbs_fullname,
+		pbs_cput    => $template_config->{pbs_cput},
+		prefix      => $prefix,
+		perl        => $template_config->{perl},
+		incdir      => $incdir,
+		daemon_name => $template_config->{daemon_name},
+		job_num     => $daemon,
+	    };
+	    $template->process( $input_file, $vars, $output_file ) or die $template->error();
+	}
+    }
 }
 
 sub DESTROY {
