3f67a7063fb712288d353aaeeb954ca4e68d6ae7
Ashton Trey Belew <abelew@gmail.com>
2009-06-19 23:01:21 -0400

    made a quick setup script for the binaries

--- setup.sh
+++ setup.sh
@@ -9,3 +9,54 @@ cd HotKnots_v2.0
 cp bin/HotKnot ../../work
 cp bin/params/* ../../bin/params
 cd ../../
+
+cd NUPACK1.2_pseudoknot/source
+make clean ; make
+cp Fold.out ../../../work/Fold.out.nopairs
+cd ../../
+
+cd NUPACK1.2_no_pseudoknot/source
+make clean ; make
+cp Fold.out ../../../work/Fold.out.boot.nopairs
+cd ../../
+
+cd rnamotif-3.0.4
+make clean ; make
+cd src
+make
+EXECS="efn2_drv efn_drv rm2ct rmfmt rmprune rnamotif"
+for i in "EXECS" do
+ cp $i ../../../work
+done
+cd ../../
+
+cd squid-1.9g
+./configure ; make
+EXECS="afetch alistat compalign compstruct revcomp seqsplit seqstat sfetch shuffle sindex sreformat translate weight"
+for i in "EXECS" do
+ cp $i ../../work
+done
+cd ..
+
+cd pknots-1.05
+cd src/squid
+make
+cp libsquid.a ..
+cd ..
+make
+cp pknots ../../../work
+cd ../../
+
+cd ViennaRNA-1.7.2
+./configure --prefix=`pwd`/../../ --bindir=`pwd`/../../work
+make
+cd Progs
+make ; make install
+cd ../Readseq
+cp readseq ../../../work
+cd ../Kinfold
+make ; make install
+cd ../../
+
+
+
