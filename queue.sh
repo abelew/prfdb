365c1f4de3ee7601f7b869ad1c47b6a9b9a7657a
Ashton Belew <abelew@f20-l2.deepthought.umd.edu>
2009-10-14 00:53:23 -0400

    Fixed up the pbs queue script

--- queue.sh
+++ queue.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-set -x
+#set -x
 USERID=`id | awk -F'(' '{print $2}' | awk -F ')' '{print $1}'`
 PARTIAL=`grep pbs_partialname prfdb.conf | awk -F= '{print $2}' | sed 's/'\''//g'`
 for arch in lin
   do
   for num in 01 02 03 04 05 06 07 08 09 10
     do
-    EXIST=`qstat | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
+    EXIST=`/usr/local/bin/qstat | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
     if [ "$EXIST" = "" ]; then
 	if [ $arch = "lin" ]; then
           qsub jobs/linux/$num
