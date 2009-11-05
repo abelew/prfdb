1968589189695ba78385b5bfd37b6d84474a755a
Ashton Belew <abelew@f20-l1.deepthought.umd.edu>
2009-11-05 13:52:43 -0500

    small queue.sh change

--- queue.sh
+++ queue.sh
@@ -5,12 +5,12 @@ USERID=`id | awk -F'(' '{print $2}' | awk -F ')' '{print $1}'`
 PARTIAL=`grep pbs_partialname prfdb.conf | awk -F= '{print $2}' | sed 's/'\''//g'`
 for arch in lin
   do
-  for num in 01 02 03 04 05 06 07 08 09 10
+  for num in {01..60}
     do
     EXIST=`/usr/local/bin/qstat | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
     if [ "$EXIST" = "" ]; then
 	if [ $arch = "lin" ]; then
-          qsub jobs/linux/$num
+          echo "qsub jobs/linux/$num"
         elif [ $arch = "aix" ]; then
           qsub jobs/aix/$num
         elif [ $arch = "iri" ]; then
