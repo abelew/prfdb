6e25fee0f7fc2bf31f8fbfc6ac86f1c9eaac0128
Ashton Belew <abelew@f20-l1.deepthought.umd.edu>
2009-11-05 15:10:04 -0500

    finished queu

--- queue.sh
+++ queue.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #set -x
 . ~/.bashrc
+QSTAT=/usr/local/torque/bin/qstat
 USERID=`id | awk -F'(' '{print $2}' | awk -F ')' '{print $1}'`
 PARTIAL=`grep pbs_partialname prfdb.conf | awk -F= '{print $2}' | sed 's/'\''//g'`
 for arch in lin
   do
   for num in {01..60}
     do
-    EXIST=`/usr/local/bin/qstat | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
+    EXIST=`$QSTAT | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
     if [ "$EXIST" = "" ]; then
 	if [ $arch = "lin" ]; then
-          echo "qsub jobs/linux/$num"
+          /usr/local/bin/qsub jobs/linux/$num
         elif [ $arch = "aix" ]; then
-          qsub jobs/aix/$num
+          /usr/local/bin/qsub jobs/aix/$num
         elif [ $arch = "iri" ]; then
-          qsub jobs/irix/$num
+          /usr/local/bin/qsub jobs/irix/$num
         fi
      else
         echo "The job $num in arch $arch is already running."
