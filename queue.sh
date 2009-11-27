a481ad2bdfc1ce0dfb90cce0b2a6861d375318f7
Ashton Belew <abelew@f20-l1.deepthought.umd.edu>
2009-11-26 19:36:51 -0500

    futzing with the queue script

--- queue.sh
+++ queue.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+. ~/.bashrc
+cd $PRFDB_HOME
 QSTAT=/usr/local/torque/bin/qstat
 USERID=`id | awk -F'(' '{print $2}' | awk -F ')' '{print $1}'`
 PARTIAL=`grep pbs_partialname prfdb.conf | awk -F= '{print $2}' | sed 's/'\''//g'`
 for arch in lin
   do
-  for num in {1..40}
+  for num in {1..70}
 
     do
     EXIST=`$QSTAT | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
@@ -21,3 +23,4 @@ for arch in lin
      fi 
      done
   done
+at 12am < queue.sh 2>/dev/null 1>&2
