cd92a29ccc248c1a9964a96e7b5b964b0ce1d842
Ashton Belew <abelew@f20-l1.deepthought.umd.edu>
2010-10-22 15:17:01 -0400

    set up a test suite which downloads missing perl modules

---
+++ lib/MyDeps.pm
@@ -0,0 +1,64 @@
+package MyDeps;
+use CPAN;
+use strict;
+use Test::More;
+#use vars qw($VERSION);
+#our @ISA = qw(Exporter);
+#our @EXPORT = qw(deps);    # Symbols to be exported by default
+
+our @deps = (
+    'Number::Format',
+    'File::Temp',
+    'DBI',
+    'AppConfig',
+    'Getopt::Long',
+    'SVG',
+    'GD::Graph',
+    'GD::Text',
+    'GD::Graph::mixed',
+    'Statistics::Basic',
+    'Statistics::Distributions',
+    'Bio::DB::Universal',
+    'Bio::Seq',
+    'Bio::SearchIO::blast',
+    'GD::SVG',
+    'Log::Log4perl',
+    );
+
+our @apache_deps = (
+    'Apache::DBI',
+    'Apache2::Request',
+    'Apache2::Upload',
+    'Apache::DBI',
+    'HTML::Mason',
+    );
+
+sub Resolve {
+    my $type = shift;
+    my @array;
+    if (!defined($type)) {
+	@array = @deps;
+    } else {
+	@array = @apache_deps;
+    }
+    $ENV{CFLAGS}="-I$ENV{PRFDB_HOME}/usr/include -L$ENV{PRFDB_HOME}/usr/lib";
+    $ENV{LD_LIBRARY_PATH}="$ENV{LD_LIBRARY_PATH}:$ENV{PRFDB_HOME}/usr/lib";
+    foreach my $d (@array) {
+	my $response = use_ok($d);
+	diag("Testing for $d\n");
+	if ($response != 1) {
+	    diag("$d appears to be missing, building in $ENV{PRFDB_HOME}/src/perl\n");
+	    CPAN->mkmyconfig;
+	    CPAN::Shell->o('autocommit', 1);
+	    CPAN::Shell->o('conf', 'build_cache', 1024000);
+	    CPAN::Shell->o('conf', 'build_dir', "$ENV{PRFDB_HOME}/src/perl");
+	    CPAN::Shell->o('conf', 'makepl_arg', "PREFIX=$ENV{PRFDB_HOME}/usr");
+	    CPAN::Shell->o('conf', 'make_install_arg', "PREFIX=$ENV{PRFDB_HOME}/usr");
+	    CPAN::Shell->o('conf', 'mbuild_install_arg', "PREFIX=$ENV{PRFDB_HOME}/usr");
+	    CPAN::Shell->o('conf', 'make_install', "PREFIX=$ENV{PRFDB_HOME}/usr make install ");
+	    CPAN::Shell->install($d);
+	}
+    }
+}
+
+1;
--- t/02_perl_requirements.t
+++ t/02_perl_requirements.t
@@ -1,23 +1,7 @@
-# Before 'make install' is performed, this script
-# should be run with 'make test'
-
+BEGIN {
+  use lib qq"$ENV{PRFDB_HOME}/lib";
+}
 use Test::More qw(no_plan);
+use MyDeps;
+MyDeps::Resolve();
 
-## First the base modules required
-use_ok(Number::Format);
-use_ok(DBI);
-use_ok(Getopt::Long);
-use_ok(File::Temp);
-use_ok(Fcntl);
-## Then the more random ones for graphing and such
-use_ok(GD::Graph);
-use_ok(GD::Text);
-use_ok(GD::SVG);
-use_ok(SVG);
-use_ok(Statistics::Basic);
-use_ok(Statistics::Distributions);
-use_ok(Bio::DB::Universal);
-use_ok(AppConfig);
-use_ok(Log::Log4perl);
-use_ok(Log::Log4perl::Level);
-use_ok(Bio::Root::Exception);
