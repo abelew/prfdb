1294c00cfcc77a315bb94fba720e779741fd4940
Ashton Belew <abelew@f20-l1.deepthought.umd.edu>
2010-11-13 12:08:24 -0500

    small changes to queue submission for pbs

--- queue.sh
+++ queue.sh
@@ -14,11 +14,11 @@ for num in $(eval echo {01..`echo -n $DAEMONS`})
     EXIST=`$QSTAT | grep $USERID | grep $PARTIAL | awk '{print $2}' | grep $arch | awk -F'_' '{print $3}' | grep $num`
     if [ "$EXIST" = "" ]; then
 	if [ $arch = "lin" ]; then
-          /usr/local/bin/qsub -m n jobs/linux/$num 2>/dev/null 1>&2 
+          /usr/local/bin/qsub -j eo -e outputs/qsub.err -m n jobs/linux/$num 2>/dev/null 1>&2 
         elif [ $arch = "aix" ]; then
-          /usr/local/bin/qsub -m n jobs/aix/$num 2>/dev/null 1>&2 
+          /usr/local/bin/qsub -j eo -e outputs/qsub.err -m n jobs/aix/$num 2>/dev/null 1>&2 
         elif [ $arch = "iri" ]; then
-          /usr/local/bin/qsub -m n jobs/irix/$num 2>/dev/null 1>&2
+          /usr/local/bin/qsub -j eo -e outputs/qsub.err -m n jobs/irix/$num 2>/dev/null 1>&2
         fi
      fi 
      done
