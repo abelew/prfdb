cabc136b8f3d43d64b4e8d7e6a2ddd97fdbc6f47
Ashton Trey Belew <abelew@umd.edu>
2012-03-11 18:41:13 -0400

    recode accessions, small changes

--- contrib/test6.pl
+++ contrib/test6.pl
@@ -6,9 +6,36 @@ use lib "$ENV{PRFDB_HOME}/lib";
 use PRFConfig;
 use PRFdb qw"AddOpen RemoveFile Callstack Cleanup";
 use PRFGraph;
-$config = new PRFConfig(config_file => "$ENV{PRFDB_HOME}/prfdb.conf");
+use RNAFolders;
+
+my $config = new PRFConfig(config_file => "$ENV{PRFDB_HOME}/prfdb.conf");
 $db = new PRFdb(config => $config);
 $SIG{INT} = \&PRFdb::Cleanup;
-my $g = new PRFGraph;
 
-$g->Graph_Agreement();
+my $fold = new RNAFolders;
+
+my $mfe_tables = $db->MySelect("SHOW TABLES LIKE \"mfe_%\"");
+my $lan_tables = $db->MySelect("SHOW TABLES LIKE \"landscape_%\"");
+my @all_tables = @{$mfe_tables};
+push(@all_tables, @{$lan_tables});
+foreach my $t (@all_tables) {
+    my $table = $t->[0];
+    print "Working on $table\n";
+    my $data = $db->MySelect("SELECT id, sequence, parens FROM $table");
+    my $count = 1;
+    foreach my $datum (@{$data}) {
+	my ($id, $sequence, $parens) = @{$datum};
+	my $mfe_turner = $fold->Compute_Energy(sequence => $sequence, parens => $parens);
+	if (!$mfe_turner) {
+	    print "cd $ENV{PRFDB_HOME}/bin && $ENV{PRFDB_HOME}/bin/computeEnergy -d $sequence \"$parens\"\n";
+	} else {
+	    print "TESTME: $mfe_turner\n";
+	}
+
+#	if (($count % 100) == 0) {
+#	    print "On $count, mfe is: $mfe_turner\n";
+#	}
+	$db->MyExecute("UPDATE $table SET mfe_turner = '$mfe_turner' WHERE id = '$id'");
+	$count++;
+    }
+}
